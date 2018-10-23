f129879352e5b288674c1f3afc7724b93be1fa3a
Felix Buehler <account@buehler.rocks>
2018-10-23 23:15:51 +0200

    [web] added overlay

--- tmcwebserver/html/leaflet_script.js
+++ tmcwebserver/html/leaflet_script.js
@@ -1,8 +1,10 @@
 var bounds = [
     [47.2, 5.8], // Southwest coordinates
-    [55.0, 15.4]  // Northeast coordinates
+    [55.1, 15.4]  // Northeast coordinates
 ];
-var map = new L.map('map').setView([48.7758, 9.1829], 13).setMaxBounds(bounds);
+var map = new L.map('map', {
+    preferCanvas: true
+}).setView([48.7758, 9.1829], 13).setMaxBounds(bounds);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
     center: map.getBounds().getCenter(),
@@ -19,9 +21,14 @@ map.doubleClickZoom.disable();
 var lat, lat_goal, lat_start, lon, lon_start, lon_goal, lat_start_short, lat_goal_short, lon_start_short, lon_goal_short;
 var geojsonFeature;
 var geoObjectName;
+var isOverlayPointsDrawn = false;
+var isOverlayLinesDrawn = false;
+var points;
+var lines;
+var url = "http://localhost:8081/";
 
 var layerlist = {};
-map.on('click', onMapClick);
+
 //Called from SetStart/SetGoal. Creates GeoJson object for the start or goal and adds it to the map.
 function createGeo(lat, lon) {
     geojsonFeature = {
@@ -51,14 +58,6 @@ function createGeo(lat, lon) {
     }).addTo(map);
 }
 
-function onMapClick(e){
-    lat = e.latlng.lat;
-    lon = e.latlng.lng;
-    var popup = L.popup()
-        .setLatLng(e.latlng)
-        .setContent(lat + ", " + lon)
-        .openOn(map);
-}
 //request
 function search() {
     console.log(map.getBounds());
@@ -103,6 +102,79 @@ function createGeoJson(json) {
         }).addTo(map);
     }
     console.log("done painting");
+}
+
+function overlay() {
+    var xhr_o = new XMLHttpRequest();
+    xhr_o.open("GET", url + "poffsets.json", true);
+    xhr_o.setRequestHeader("Content-type", "application/json");
+    xhr_o.onreadystatechange = function () {
+        if (xhr_o.readyState === 4 && xhr_o.status === 200) {
+            var json = JSON.parse(xhr_o.responseText);
+            DrawOverlayLines(json);
+        }
+    };
+    xhr_o.send();
+}
+
+function overlay_points() {
+    var xhr_p = new XMLHttpRequest();
+    xhr_p.open("GET", url + "points.json", true);
+    xhr_p.setRequestHeader("Content-type", "application/json");
+    xhr_p.onreadystatechange = function () {
+        if (xhr_p.readyState === 4 && xhr_p.status === 200) {
+            var json = JSON.parse(xhr_p.responseText);
+            DrawOverlayPoints(json);
+        }
+    };
+    xhr_p.send();
+}
+
+function overlay_points() {
+    var xhr_p = new XMLHttpRequest();
+    xhr_p.open("GET", url + "points.json", true);
+    xhr_p.setRequestHeader("Content-type", "application/json");
+    xhr_p.onreadystatechange = function () {
+        if (xhr_p.readyState === 4 && xhr_p.status === 200) {
+            var json = JSON.parse(xhr_p.responseText);
+            DrawOverlayPoints(json);
+        }
+    };
+    xhr_p.send();
+}
+
+function DrawOverlayLines(json) {
+    if (!isOverlayLinesDrawn) {
+        isOverlayLinesDrawn = true;
+        for (var i = 0; i < json.length; i++) {
+            L.polyline(json[i], {
+                weight: 2,
+                color: 'blue'
+            }).addTo(map);
+        }
+    }
+    console.log("done painting lines");
+    overlay_points();
+}
+
+function DrawOverlayPoints(json) {
+    if (!isOverlayPointsDrawn) {
+        isOverlayPointsDrawn = true;
+        for (var i = 0; i < json.length; i++) {
+            L.circleMarker(json[i]["coordinates"], {
+                radius: 1,
+                color: 'red'
+            }).addTo(map).bindPopup('lcd ' + json[i]["lcd"]);
+        }
+    }
+    console.log("done painting points");
+}
+
+function onEachFeature(feature, layer) {
+    // does this feature have a property named popupContent?
+    if (feature.properties && feature.properties.popupContent) {
+        layer.bindPopup(feature.properties.popupContent);
+    }
 }
 
 var heat = L.heatLayer([
---
+++ tmcwebserver/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./webserver 0.0.0.0 8081 html/ 1
